41a3388befeaa5fa281e5bbde0145da5947d70ea
Hegyközi Petra <petra.hegykozi12345@gmail.com>
2024-10-05 16:25:35 +0200

    A Form-ban lévő elemek (label, gomb) elnevezéseinek megváltoztatása

--- TODOLIST/TODOLIST/Form1.Designer.cs
+++ TODOLIST/TODOLIST/Form1.Designer.cs
@@ -28,143 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.Todolist_label = new System.Windows.Forms.Label();
+            this.Cim_label = new System.Windows.Forms.Label();
+            this.Leiras_label = new System.Windows.Forms.Label();
+            this.Cim_textBox = new System.Windows.Forms.TextBox();
+            this.Leiras_textBox = new System.Windows.Forms.TextBox();
+            this.ujTeendoButton = new System.Windows.Forms.Button();
+            this.szerkesztesButton = new System.Windows.Forms.Button();
+            this.torlesButton = new System.Windows.Forms.Button();
+            this.mentesButton = new System.Windows.Forms.Button();
+            this.checkedListBox = new System.Windows.Forms.CheckedListBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
-            // label1
+            // Todolist_label
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Nirmala UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(344, 21);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(147, 37);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "To-Do List";
+            this.Todolist_label.AutoSize = true;
+            this.Todolist_label.Font = new System.Drawing.Font("Nirmala UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Todolist_label.Location = new System.Drawing.Point(344, 21);
+            this.Todolist_label.Name = "Todolist_label";
+            this.Todolist_label.Size = new System.Drawing.Size(147, 37);
+            this.Todolist_label.TabIndex = 0;
+            this.Todolist_label.Text = "To-Do List";
             // 
-            // label2
+            // Cim_label
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(35, 92);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 21);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Cím";
+            this.Cim_label.AutoSize = true;
+            this.Cim_label.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cim_label.Location = new System.Drawing.Point(35, 92);
+            this.Cim_label.Name = "Cim_label";
+            this.Cim_label.Size = new System.Drawing.Size(40, 21);
+            this.Cim_label.TabIndex = 1;
+            this.Cim_label.Text = "Cím";
             // 
-            // label3
+            // Leiras_label
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(35, 157);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(54, 21);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Leírás";
+            this.Leiras_label.AutoSize = true;
+            this.Leiras_label.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Leiras_label.Location = new System.Drawing.Point(35, 157);
+            this.Leiras_label.Name = "Leiras_label";
+            this.Leiras_label.Size = new System.Drawing.Size(54, 21);
+            this.Leiras_label.TabIndex = 2;
+            this.Leiras_label.Text = "Leírás";
             // 
-            // textBox1
+            // Cim_textBox
             // 
-            this.textBox1.Font = new System.Drawing.Font("Nirmala UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(39, 116);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(670, 23);
-            this.textBox1.TabIndex = 3;
+            this.Cim_textBox.Font = new System.Drawing.Font("Nirmala UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cim_textBox.Location = new System.Drawing.Point(39, 116);
+            this.Cim_textBox.Name = "Cim_textBox";
+            this.Cim_textBox.Size = new System.Drawing.Size(670, 23);
+            this.Cim_textBox.TabIndex = 3;
             // 
-            // textBox2
+            // Leiras_textBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(39, 181);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(670, 20);
-            this.textBox2.TabIndex = 4;
+            this.Leiras_textBox.Font = new System.Drawing.Font("Nirmala UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Leiras_textBox.Location = new System.Drawing.Point(39, 181);
+            this.Leiras_textBox.Name = "Leiras_textBox";
+            this.Leiras_textBox.Size = new System.Drawing.Size(670, 23);
+            this.Leiras_textBox.TabIndex = 4;
             // 
-            // button1
+            // ujTeendoButton
             // 
-            this.button1.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(12, 252);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(181, 31);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Új Teendő";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.ujTeendoButton.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ujTeendoButton.Location = new System.Drawing.Point(12, 252);
+            this.ujTeendoButton.Name = "ujTeendoButton";
+            this.ujTeendoButton.Size = new System.Drawing.Size(181, 31);
+            this.ujTeendoButton.TabIndex = 5;
+            this.ujTeendoButton.Text = "Új Teendő";
+            this.ujTeendoButton.UseVisualStyleBackColor = true;
+            this.ujTeendoButton.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // szerkesztesButton
             // 
-            this.button2.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(209, 252);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(181, 31);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "Szerkesztés";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.szerkesztesButton.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.szerkesztesButton.Location = new System.Drawing.Point(209, 252);
+            this.szerkesztesButton.Name = "szerkesztesButton";
+            this.szerkesztesButton.Size = new System.Drawing.Size(181, 31);
+            this.szerkesztesButton.TabIndex = 6;
+            this.szerkesztesButton.Text = "Szerkesztés";
+            this.szerkesztesButton.UseVisualStyleBackColor = true;
+            this.szerkesztesButton.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
+            // torlesButton
             // 
-            this.button3.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(413, 252);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(181, 31);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "Törlés";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.torlesButton.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.torlesButton.Location = new System.Drawing.Point(413, 252);
+            this.torlesButton.Name = "torlesButton";
+            this.torlesButton.Size = new System.Drawing.Size(181, 31);
+            this.torlesButton.TabIndex = 7;
+            this.torlesButton.Text = "Törlés";
+            this.torlesButton.UseVisualStyleBackColor = true;
+            this.torlesButton.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button4
+            // mentesButton
             // 
-            this.button4.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(607, 252);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(181, 31);
-            this.button4.TabIndex = 8;
-            this.button4.Text = "Mentés";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.mentesButton.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mentesButton.Location = new System.Drawing.Point(607, 252);
+            this.mentesButton.Name = "mentesButton";
+            this.mentesButton.Size = new System.Drawing.Size(181, 31);
+            this.mentesButton.TabIndex = 8;
+            this.mentesButton.Text = "Mentés";
+            this.mentesButton.UseVisualStyleBackColor = true;
+            this.mentesButton.Click += new System.EventHandler(this.button4_Click);
             // 
-            // checkedListBox1
+            // checkedListBox
             // 
-            this.checkedListBox1.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(12, 306);
-            this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(776, 214);
-            this.checkedListBox1.TabIndex = 10;
+            this.checkedListBox.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedListBox.FormattingEnabled = true;
+            this.checkedListBox.Location = new System.Drawing.Point(12, 306);
+            this.checkedListBox.Name = "checkedListBox";
+            this.checkedListBox.Size = new System.Drawing.Size(776, 204);
+            this.checkedListBox.TabIndex = 10;
             // 
-            // dateTimePicker1
+            // dateTimePicker
             // 
-            this.dateTimePicker1.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePicker1.Location = new System.Drawing.Point(39, 216);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(228, 25);
-            this.dateTimePicker1.TabIndex = 11;
+            this.dateTimePicker.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker.Location = new System.Drawing.Point(39, 216);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(228, 25);
+            this.dateTimePicker.TabIndex = 11;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 556);
-            this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.checkedListBox1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.checkedListBox);
+            this.Controls.Add(this.mentesButton);
+            this.Controls.Add(this.torlesButton);
+            this.Controls.Add(this.szerkesztesButton);
+            this.Controls.Add(this.ujTeendoButton);
+            this.Controls.Add(this.Leiras_textBox);
+            this.Controls.Add(this.Cim_textBox);
+            this.Controls.Add(this.Leiras_label);
+            this.Controls.Add(this.Cim_label);
+            this.Controls.Add(this.Todolist_label);
             this.Name = "Form1";
-            this.Text = "          ";
+            this.Text = "To-Do List";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -172,17 +173,17 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.CheckedListBox checkedListBox1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label Todolist_label;
+        private System.Windows.Forms.Label Cim_label;
+        private System.Windows.Forms.Label Leiras_label;
+        private System.Windows.Forms.TextBox Cim_textBox;
+        private System.Windows.Forms.TextBox Leiras_textBox;
+        private System.Windows.Forms.Button ujTeendoButton;
+        private System.Windows.Forms.Button szerkesztesButton;
+        private System.Windows.Forms.Button torlesButton;
+        private System.Windows.Forms.Button mentesButton;
+        private System.Windows.Forms.CheckedListBox checkedListBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
     }
 }
 
